e9b0ba9534d1fc7db1ecbc24c533d656d2f289fb
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-12-06 10:24:11 -0700

    Recommit.

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -50,12 +50,16 @@ const Signup = () => {
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                            <div class="col-span-6 sm:col-span-3">
+                                <label for="first-name" class="block text-sm font-medium text-gray-700">Username</label>
+                                <input onChange={handleFormChange} type="text" name="username" id="first_name" autocomplete="username" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
+                            </div>
+                            {/* <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
                             <select id="username" name="country" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                             <option>United States</option>
                             <option>Canada</option>
                             <option>Mexico</option>
-                            </select>
+                            </select> */}
                         </div>
 
                         <div class="col-span-6">
