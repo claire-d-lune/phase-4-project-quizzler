4cffc3f9b0048980f8df3f9a9e1395d8ed334f7e
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-12-05 22:49:42 -0700

    updating some work I completed today. A lot of it was just trouble shooting tailwind, but the signup and log in pages have a basic structure now.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import NewUserForm from './components/NewUserForm';
 import QuizPage from './components/QuizPage';
 import User from './components/User';
+import SignUp from './components/Signup';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route exact path='/login' element= {<NewUserForm/>}/>
           <Route path='/user' element= {<User/>}/>
           <Route path='/quizpage' element= {<QuizPage/>}/>
+          <Route path='/sign_up' element = {<SignUp/>}/>
           {/* <Route path='/nav' element= {<Nav/>}/> */}
         </Routes>
     </div>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
   theme: {
     extend: {},
   },
